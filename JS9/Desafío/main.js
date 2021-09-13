aa89b1baba7f29c12c96964cc1bd86a83ea983d2
Feliciotti <feliciotti@hotmail.com>
2021-09-13 20:28:23 -0300

    Desafío9

---
+++ JS9/Desafío/main.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById('btn')
+btn.addEventListener("click", function(){
+    alert("No vuelva a hacer eso.")
+})
+
+let input = document.getElementById('input')
+input.addEventListener("change", function(){
+    console.log("Si esto no cumple la tarea me muero.")
+})
